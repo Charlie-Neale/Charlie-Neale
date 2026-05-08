8a213e912666adabd63f514edae0cfec27d61965
Charlie Neale <charlieneale0@gmail.com>
2026-05-07 20:32:27 -0400

    fix: remove basePath for user GitHub Pages site

--- Website/next.config.mjs
+++ Website/next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Charlie-Neale',
-  assetPrefix: '/Charlie-Neale/',
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+
